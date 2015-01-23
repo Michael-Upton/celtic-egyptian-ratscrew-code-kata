e65568a65cd984c756594500076962190880c703
Michael-Upton <Michael.Upton@red-gate.com>
2015-01-23 12:02:58 +0000

    Forgot to add this too

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/DarkQueenSnapper.cs
@@ -0,0 +1,10 @@
+﻿namespace CelticEgyptianRatscrewKata
+{
+    public class DarkQueenSnapper : ISnapValidator
+    {
+        public bool CanSnap(Stack cards)
+        {
+            return cards.TopCard.Equals(Rank.Queen.Of(Suit.Spades));
+        }
+    }
+}
