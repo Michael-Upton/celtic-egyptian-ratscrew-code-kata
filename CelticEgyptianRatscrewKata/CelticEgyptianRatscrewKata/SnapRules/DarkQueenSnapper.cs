1875efe27d8eece83a509c3cc34b79fc36cf422e
Michael-Upton <Michael.Upton@red-gate.com>
2015-02-05 09:50:31 +0000

    Move Snappers into their own folder

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SnapRules/DarkQueenSnapper.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SnapRules/DarkQueenSnapper.cs
@@ -1,4 +1,4 @@
-﻿namespace CelticEgyptianRatscrewKata
+﻿namespace CelticEgyptianRatscrewKata.SnapRules
 {
     public class DarkQueenSnapper : ISnapValidator
     {
--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapValidatorTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using CelticEgyptianRatscrewKata.SnapRules;
 using NUnit.Framework;
 
 namespace CelticEgyptianRatscrewKata.Tests
