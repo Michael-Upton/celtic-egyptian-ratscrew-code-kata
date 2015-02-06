50dd9acdba459f621a0ccb56f9b21315e7f82504
Michael-Upton <Michael.Upton@red-gate.com>
2015-02-06 09:59:08 +0000

    [Red] Failining tests

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameBuilderTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    [TestFixture]
+    public class GameBuilderTests
+    {
+        [Test]
+        public void CantStartAGameWithoutPlayers()
+        {
+            var gameBuilder = new GameBuilder();
+            Assert.Throws<InvalidOperationException>(() => gameBuilder.Start());
+        }
+
+        [Test]
+        public void CanStartAGameWithOnePlayer()
+        {
+            var gameBuilder = new GameBuilder();
+            
+            gameBuilder.AddPlayer("Michael");
+            var game = gameBuilder.Start();
+
+            Assert.That(game.Players["Michael"], Is.Not.Null);
+        }
+
+    }
+}
